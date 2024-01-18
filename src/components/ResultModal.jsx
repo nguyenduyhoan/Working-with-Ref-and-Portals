b884b20428b9085422990ee8331e660c3b45bc94
Your Name <you@example.com>
2024-01-18 15:47:32 +0700

    ForwardRef

---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,18 @@
+import { forwardRef } from "react"
+
+const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+    return (
+        <dialog ref={ref} className="result-modal" open>
+            <h2>You {result}</h2>
+            <p>The target time was <strong>{targetTime} seconds.</strong></p>
+            <p>You stopped the timer with <trong>X seconds left.</trong></p>
+            <form method="dialog">
+                <button>
+                    Close
+                </button>
+            </form>
+        </dialog>
+    )
+})
+
+export default ResultModal;
