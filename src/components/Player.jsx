279f05fbbf6c625256dd5b2d3a483fa0c4fda8ff
Your Name <you@example.com>
2024-01-18 13:15:17 +0700

    working with refs

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const [inputPlayer, setInputPlayer] = useState(null);
+  const playerName = useRef();
+
+  function inputOnChange(event) {
+    setInputPlayer(playerName.current.value);
+    playerName.current.value = '';
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>{inputPlayer ?? 'Welcome unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={playerName} />
+        <button onClick={inputOnChange}>Set Name</button>
       </p>
     </section>
   );
